8ad99bfe42115528a8f9142dfa51b4f99178b801
jugalpanchal <jugalpanchal@Lapi-Jugal-3558>
2016-05-19 00:02:19 +0530

    Get Car object from Application Context and perform set/get operation.

--- springsimpleprototype/src/com/jugalpanchal/springframework/springsimpleprototype/MainApp.java
+++ springsimpleprototype/src/com/jugalpanchal/springframework/springsimpleprototype/MainApp.java
@@ -1,10 +1,16 @@
 package com.jugalpanchal.springframework.springsimpleprototype;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	    Car car = (Car) context.getBean("car");
 
+	    String name = car.getName();
+	    System.out.println("Car Name : " + name);
 	}
 
 }
