4639f3972170d24e0ee960915196eb2ddc680b09
jugalpanchal <jugalpanchal@Lapi-Jugal-3558>
2016-05-18 23:57:35 +0530

    Return name from getter

--- springsimpleprototype/src/com/jugalpanchal/springframework/springsimpleprototype/Car.java
+++ springsimpleprototype/src/com/jugalpanchal/springframework/springsimpleprototype/Car.java
@@ -1,13 +1,13 @@
 package com.jugalpanchal.springframework.springsimpleprototype;
 
 public class Car {
-	   private String name;
+	private String name;
 
-	   public void setName(String name){
-	      this.name  = name;
-	   }
+	public void setName(String name){
+		this.name  = name;
+	}
 
-	   public void getName(){
-	      System.out.println("Your Name : " + name);
-	   }
+	public String getName(){
+		return this.name;
+	}
 }
