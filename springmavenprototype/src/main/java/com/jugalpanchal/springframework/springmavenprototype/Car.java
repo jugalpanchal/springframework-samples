21a97e553e94c0f8959f1234cbce5658051d0fd1
jugalpanchal <jugalpanchal@Lapi-Jugal-3558>
2016-05-19 19:53:40 +0530

    Added Car Class

---
+++ springmavenprototype/src/main/java/com/jugalpanchal/springframework/springmavenprototype/Car.java
@@ -0,0 +1,13 @@
+package com.jugalpanchal.springframework.springmavenprototype;
+
+public class Car {
+	private String name;
+
+	public void setName(String name){
+		this.name  = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+}
